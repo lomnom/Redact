00ea748b12073aef158ac1512578c13cf36e7015
lomnom <zhaoxiong.ang@gmail.com>
2025-01-03 23:42:21 +0800

    use drag.

--- Censor.py
+++ Censor.py
@@ -2,12 +2,37 @@ import wx
 import pyautogui as pag
 pag.PAUSE = 0
 
-# The first run is slow and would cause issues downstream if this is not done (ugly solution)
-pag.move(1, 0) 
-pag.move(-1, 0)
+NOTHINGFUNC = lambda *args, **kwargs: None
+class Element: 
+	def __init__(self, name):
+		self.name = name
+		for part in ["getRegion", "onClick", "onDrag", "render"]:
+			self.newPart(part)
+
+	# getRegion(self, frame) returns [x, y, h, w] within which it is considered to be within the element
+	# onClick(self, frame, event) is called on a left click
+	# onDrag(self, frame, event, previous, start, end, dragLength) is called after the first drag event.
+	# render(self, frame, buffer) is called when to render the element when window is in focus.
+
+	def newPart(self, name):
+		setattr(self, '_'+name, NOTHINGFUNC)
+		def newPartDecorator(func):
+			nonlocal self, name
+			setattr(self, '_'+name, func)
+
+		def newPartCaller(*args, **kwargs): # Note: slow?
+			nonlocal self, name
+			return getattr(self, '_'+name)(self, *args, **kwargs)
+
+		setattr(self, name+"Call", newPartDecorator)
+		setattr(self, name, newPartCaller)
+
+	def __repr__(self): # Not compliant but eh
+		return "E-" + self.name
 
 class Censor(wx.Frame):
 	censors = []
+	elements = []
 	@classmethod
 	def censor(cls, function):
 		cls.censors.append([function.__name__, function])
@@ -16,12 +41,12 @@ class Censor(wx.Frame):
 		style = ( wx.CLIP_CHILDREN | wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR |
 				  wx.NO_BORDER | wx.FRAME_SHAPED  )
 		wx.Frame.__init__(self, None, title='Censor', style = style)
-		self.dragPos = None
-		self.Bind(wx.EVT_KEY_UP, self.OnKeyUp)
 		self.Bind(wx.EVT_MOTION, self.OnMouse)
-		self.Bind(wx.EVT_ENTER_WINDOW, self.OnEnterWindow)
-		self.Bind(wx.EVT_LEAVE_WINDOW, self.OnLeaveWindow)
+		# self.Bind(wx.EVT_ENTER_WINDOW, self.OnEnterWindow)
+		# self.Bind(wx.EVT_LEAVE_WINDOW, self.OnLeaveWindow)
 		self.Bind(wx.EVT_PAINT, self.OnPaint)
+		self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+		self.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
 		self.Show(True)
 
 		height = 50 # Initial dimensions
@@ -35,153 +60,147 @@ class Censor(wx.Frame):
 		screenSize = wx.DisplaySize() # Place at center of screen
 		self.SetPosition((screenSize[0]//2 - width//2, screenSize[1]//2 - height//2))
 
-		self.insideBox = None # If the cursor is inside the box for the long term, not just entering to shrink
-		self.lastChange = None # Used by shrinking overrun & checking expansion overrun
+		self.dragStartPos = None
+		self.dragLength = 0
+		self.dragElements = None
+		self.dragPrevPos = None
 
-	def OnKeyUp(self, event):
-		event.Skip() # Ignore keyboard inputs.
+	def resize(self, newSize):
+		self.SetSize((max(self.minSize, newSize.width), max(self.minSize, newSize.height)))
+		size = self.GetSize()
+		self.buffer = wx.Image(size)
+		self.rSize, self.bSize = self.getRegionSize(size)
 
-	def handleSquare(self):
-		self.currentCensor = (self.currentCensor + 1) % len(self.censors)
-		print(f"Now using censor {self.currentCensor}")
-		self.OnPaint()
+	@staticmethod
+	def regionSteps(dimension):
+		if dimension >= 30: size = 20 
+		elif dimension > 20: size = 10
+		else: size = 5
+		return size
+
+	def getRegionSize(self, size):
+		return (self.regionSteps(size.width), self.regionSteps(size.height))
 
 	def OnPaint(self, event=None):
-		self.censors[self.currentCensor][1](self, event)
+		dc = wx.PaintDC(self)
+		dc.Clear()
 
-	def resize(self, newSize, render = True):
-		self.SetSize((max(self.minSize, newSize.width), max(self.minSize, newSize.height)))
-		self.buffer = wx.Image(self.GetSize())
+		self.censors[self.currentCensor][1](self, event, self.buffer)
+		if self.HasFocus():
+			for element in self.elements:
+				element.render(self, self.buffer)
 
-	def getRegionSize(self, size, bigger = False):
-		# Width of a region in the right side
-		if bigger and size.width >= 60: rSize = 40 
-		elif size.width >= 30: rSize = 20 
-		elif size.width > 20: rSize = 10
-		else: rSize = 5
+		dc.DrawBitmap(wx.Bitmap(self.buffer), 0, 0) # Commit the buffer
 
-		# Width of a region in the bottom side
-		if bigger and size.height >= 60: bSize = 40 
-		elif size.height >= 30: bSize = 20 
-		elif size.height > 20: bSize = 10
-		else: bSize = 5
-		return (rSize, bSize)
-
-	def OnEnterWindow(self, event):
-		print(f"Entered, {event.GetPosition()}")
-		if self.dragPos is not None: # Ignore everything which happens during a drag to avoid resizing
-			return
-		insideBox = True 
-		size = self.GetSize()
-		newSize = self.GetSize()
-		mouse = event.GetPosition()
-		rSize, bSize = self.getRegionSize(size, bigger = True) # Make it bigger to reduce overruns
-
-		#Bottom right square
-		if size.width - rSize <= mouse.x <= size.width and size.height - bSize <= mouse.y <= size.height:
-			insideBox = False
-			pag.move(rSize, bSize) # Teleport cursor away from the box
-			self.handleSquare()
-		else:
-			if size.width-rSize <= mouse.x <= size.width: # Right side shrink
-				newSize.width = size.width - 10
-				insideBox = False
-				self.lastChange = "r-"
-				self.resize(newSize)
-				print("Shrinking right")
-			elif size.height-bSize <= mouse.y <= size.height: # Bottom side shrink
-				newSize.height = size.height - 10
-				insideBox = False
-				self.lastChange = "b-"
-				self.resize(newSize)
-				print("Shrinking bottom")
-
-		if insideBox:
-			print("Now inside.")
-		self.insideBox = insideBox
-
-	def OnLeaveWindow(self, event):
-		print("Left")
-		if self.dragPos is not None: # Ignore everything which happens during a drag to avoid resizing
-			return
-		size = self.GetSize()
-		newSize = self.GetSize()
-		mouse = event.GetPosition()
-		rSize, bSize = self.getRegionSize(size)
-
-		# Expansion overruns
-		# Ignore if leaving window in shrinking & ignore if leaving the bottom right square
-		# Last change must be an expansion or it is probably a late resize.
-		if self.lastChange and self.lastChange[1] == '+' and self.insideBox: 
-			if size.width - rSize <= mouse.x: # Right side expand overrun
-				newSize.width += 60
-				self.resize(newSize)
-				# print("Expansion overrun right")
-			if size.height - bSize <= mouse.y: # Bottom side expand overrun
-				newSize.height += 60
-				self.resize(newSize)
-				# print("Expansion overrun bottom")
-
-		self.insideBox = False
+	def elementsHit(self, x, y):
+		results = []
+		for element in self.elements:
+			sx, sy, h, w = element.getRegion(self)
+			ex, ey = (sx + w, sy + h)
+			if sx <= x < ex and sy <= y < ey:
+				results.append(element)
+		return results
 
 	def OnMouse(self, event):
 		# Moving the window
+		spot = event.GetPosition()
 		if event.Dragging():
-			if not self.dragPos:
-				self.dragPos = event.GetPosition()
+			print(f"Dragging, {spot}, {self.dragLength}, {self.dragElements}")
+			self.dragLength += 1 # Number of drag events part of this drag, including this event
+			if not self.dragStartPos: # First event
+				self.dragStartPos = spot
+				self.dragElements = self.elementsHit(spot.x, spot.y)
 			else:
-				pos = event.GetPosition()
-				displacement = self.dragPos - pos
-				self.SetPosition(self.GetPosition() - displacement)
-			return
+				for element in self.dragElements:
+					element.onDrag(self, event, self.dragPrevPos, self.dragStartPos, spot, self.dragLength)
+			self.dragPrevPos = spot
 		else:
-			self.dragPos = None
+			self.dragStartPos = None
+			self.dragElements = None
+			self.dragLength = 0
+			self.dragPrevPos = None
 
-		size = self.GetSize()
-		newSize = self.GetSize()
-		mouse = event.GetPosition()
-		rSize, bSize = self.getRegionSize(size)
-		if self.insideBox: # Expanding
-			# Note that the bottom right square expands both from inside.
-			if size.width - rSize <= mouse.x <= size.width: # Right side expand
-				newSize.width = size.width + 10
-				self.resize(newSize)
-				self.lastChange = "r+"
-				# print("Growing right")
-			if size.height - bSize <= mouse.y <= size.height: # Bottom side expand
-				newSize.height = size.height + 10
-				self.resize(newSize)
-				self.lastChange = "b+"
-				# print("Growing bottom")
-		else: # Shrinking extensions. 
-			# TODO: Overruns where the cursor teleports past the trigger region are not handled.
-			# TODO: Going in from the far right of the top side at an angle directly downwards causes a huge shrink
-			if self.lastChange == "r-" and size.width != self.minSize:
-				newSize.width -= 3 * (size.width-mouse.x) + 30
-				self.resize(newSize)
-				# print("Shrink overrun right")
-			elif self.lastChange == "b-" and size.height != self.minSize:
-				newSize.height -= 3 * (size.height-mouse.y) + 30
-				self.resize(newSize)
-				# print("Shrink overrun bottom")
+	def OnLeftUp(self, event):
+		spot = event.GetPosition()
+		if self.dragLength: # Ignore ending a drag
+			if not (spot == self.dragStartPos and self.dragLength == 1): # Unless it is a 0-pixel accidental drag
+				return
+		elementsHit = self.elementsHit(spot.x, spot.y)
+		for element in elementsHit:
+			element.onClick(self, event)
+
+	def OnRightDown(self, event):
+		self.currentCensor = (self.currentCensor + 1) % len(self.censors)
+		print(f"Now using censor {self.currentCensor}, {self.censors[self.currentCensor][0]}")
+		self.OnPaint()
+
+	# def OnEnterWindow(self, event):
+	# 	pass
+
+	# def OnLeaveWindow(self, event):
+	# 	pass
+
+# Let the frame be dragged around
+drag = Element("Dragger")
+Censor.elements.append(drag)
+@drag.getRegionCall
+def getRegion(self, frame):
+	size = frame.GetSize()
+	return [0, 0, size.height - frame.bSize, size.width - frame.rSize]
+
+@drag.onDragCall
+def onDrag(self, frame, event, previous, start, end, dragLength):
+	delta = end - start
+	oldPos = frame.GetPosition()
+	frame.SetPosition(oldPos + delta)
+
+rResize = Element(f"Right resize")
+Censor.elements.append(rResize)
+@rResize.getRegionCall
+def getRegion(self, frame):
+	size = frame.GetSize()
+	return [size.width - frame.rSize, 0, size.height, frame.rSize]
+
+@rResize.onDragCall
+def onDrag(self, frame, event, previous, start, end, dragLength):
+	delta = end - previous
+	size = frame.GetSize()
+	size.width += delta.x
+	frame.resize(size)
+
+# Very similar code
+bResize = Element(f"Bottom resize")
+Censor.elements.append(bResize)
+@bResize.getRegionCall
+def getRegion(self, frame):
+	size = frame.GetSize()
+	return [0, size.height - frame.bSize, frame.bSize, size.width]
+
+@bResize.onDragCall
+def onDrag(self, frame, event, previous, start, end, dragLength):
+	delta = end - previous
+	size = frame.GetSize()
+	size.height += delta.y
+	frame.resize(size)
+
+close = Element("Close")
+Censor.elements.append(close)
+@close.getRegionCall
+def getRegion(self, frame):
+	return [0, 0, 10, 10]
+
+@close.onClickCall
+def onClick(self, frame, event):
+	print("Close button pressed. Closing.")
+	quit() # TODO: is this correct?
 
 @Censor.censor
-def black(self, event):
-	dc = wx.PaintDC(self)
-	dc.Clear()
-	size = self.GetSize()
-	self.buffer.Clear()
-
-	dc.DrawBitmap(wx.Bitmap(self.buffer), 0, 0)
+def black(frame, event, buffer):
+	buffer.Clear()
 
 @Censor.censor
-def white(self, event):
-	dc = wx.PaintDC(self)
-	dc.Clear()
-	size = self.GetSize()
-	self.buffer.Clear(value = bytes([255]))
-
-	dc.DrawBitmap(wx.Bitmap(self.buffer), 0, 0)
+def white(frame, event, buffer):
+	buffer.Clear(value = bytes([255]))
 
 app = wx.App()
 f = Censor()
